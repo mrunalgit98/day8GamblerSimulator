11787641bd42c84ca727cfd8f07da58448946dd4
mrunal <mrunalbhoir100@gmail.com>
2022-08-06 23:53:26 +0530

    UC2 As a Gambler make bet so either win or loose

--- src/Gambler.java
+++ src/Gambler.java
@@ -5,6 +5,14 @@ public class Gambler {
 
     public static void main(String[] args) {
         System.out.println("welcome to gambling simulator");
+
+        int random=(int) Math.floor(Math.random()*10%2);
+
+        if(random==bet){
+            System.out.println("win");
+        }else{
+            System.out.println("loose");
+        }
     }
 
 }
