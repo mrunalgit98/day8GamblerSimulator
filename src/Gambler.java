2848d18806e513765fa03ded2419f6827f5f1793
mrunal <mrunalbhoir100@gmail.com>
2022-08-07 00:35:13 +0530

    Each month would like to know the days won and lost and by how much

--- src/Gambler.java
+++ src/Gambler.java
@@ -6,22 +6,30 @@ public class Gambler {
     public static void main(String[] args) {
         System.out.println("welcome to gambling simulator");
         int stake=everyday;
+        int month=1;
+        while (month<=12){
+            int day=1;
+            System.out.println("month " + month);
+            while (day<=30){
+                stake=everyday;
+                int random=(int)Math.floor(Math.random()*10)%2;
+                if (random == bet) {
+                    stake = stake + 1;
+                    System.out.println("You Win.");
+                } else {
+                    stake = stake - 1;
+                    System.out.println("You Loose.");
+                }
+                if(stake<everyday){
+                    System.out.println("day " + day +"lost " +(everyday-stake)+"rs");
+                }else {
+                    System.out.println("day " + day +"lost " +(stake-everyday)+"rs");
 
-        for(int i=0;i<=20;i++){
-            int random=(int)Math.floor(Math.random()*10)%2;
-
-            if(random==bet){
-                stake=stake+1;
-                System.out.println("win");
-            }else {
-                stake=stake-1;
+                }
+                day++;
             }
-                System.out.println("loose");
-        }
-        if(stake<everyday){
-            System.out.println("after 20 day player lost " +(everyday-stake)+"rs");
-        }else {
-            System.out.println("after 20 day player won " +(stake-everyday)+"rs");
+            month++;
+            System.out.println();
         }
     }
 
