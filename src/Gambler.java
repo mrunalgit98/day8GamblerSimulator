6e71405241433a71016c1e07b68c33648ce83930
mrunal <mrunalbhoir100@gmail.com>
2022-08-07 00:08:14 +0530

    UC3 Gambler if won or lost 50% of the stake, would resign for the day

--- src/Gambler.java
+++ src/Gambler.java
@@ -6,12 +6,23 @@ public class Gambler {
     public static void main(String[] args) {
         System.out.println("welcome to gambling simulator");
 
-        int random=(int) Math.floor(Math.random()*10%2);
 
-        if(random==bet){
-            System.out.println("win");
-        }else{
-            System.out.println("loose");
+        int stake=everyday;
+        while (stake>50 &&stake<150){
+            int random=(int)Math.floor(Math.random()*10)%2;
+
+            if(random==bet){
+                stake=stake+1;
+                System.out.println("win");
+            }else {
+                stake=stake+1;
+                System.out.println("loose");
+            }
+            if(stake==50){
+                System.out.println("player resigned due to loosing"+stake);
+            }else {
+                System.out.println("player resigned due to winning" + stake);
+            }
         }
     }
 
