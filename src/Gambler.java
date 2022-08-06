49ca5d066b1c993fa59a734549d24cb8e968bea2
mrunal <mrunalbhoir100@gmail.com>
2022-08-06 23:46:44 +0530

    gambler simulator

---
+++ src/Gambler.java
@@ -0,0 +1,10 @@
+public class Gambler {
+
+    static final int everyday=100;
+    static final int bet=1;
+
+    public static void main(String[] args) {
+        System.out.println("welcome to gambling simulator");
+    }
+
+}
