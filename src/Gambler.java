533adc15a956d20135341075cfb1e1cbb6eb9588
mrunal <mrunalbhoir100@gmail.com>
2022-08-07 00:50:15 +0530

    UC7 Would also like to know my luckiest day where I won maximum and my unluckiest day where I lost maximum

--- src/Gambler.java
+++ src/Gambler.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Gambler {
 
     static final int everyday=100;
@@ -5,32 +7,50 @@ public class Gambler {
 
     public static void main(String[] args) {
         System.out.println("welcome to gambling simulator");
-        int stake=everyday;
-        int month=1;
-        while (month<=12){
-            int day=1;
-            System.out.println("month " + month);
-            while (day<=30){
-                stake=everyday;
-                int random=(int)Math.floor(Math.random()*10)%2;
-                if (random == bet) {
-                    stake = stake + 1;
-                    System.out.println("You Win.");
-                } else {
-                    stake = stake - 1;
-                    System.out.println("You Loose.");
-                }
-                if(stake<everyday){
-                    System.out.println("day " + day +"lost " +(everyday-stake)+"rs");
-                }else {
-                    System.out.println("day " + day +"lost " +(stake-everyday)+"rs");
+        int Stake = everyday;
+        int month = 1;
+        int day = 1;
+        int UnluckyDay = 1;
+        int LuckyDay = 1;
+        int UnluckyMonth= 1;
+        int LuckyMonth = 1;
+        int LuckyDayMoney = everyday;
+        int UnluckyDayMoney = everyday;
 
+        while (month<=12){
+            day=1;
+            while (day<=30){
+                Stake=everyday;
+                Random r=new Random();
+                int daily=r.nextInt(10);
+                for (int i=0;i<daily;i++){
+                    int check=(int) Math.floor(Math.random()*10)%2;
+
+                    if(check==bet){
+                        Stake=Stake+1;
+                        System.out.println("win");
+                    }else {
+                        Stake=Stake-1;
+                        System.out.println("loose");
+                    }
+                }
+                if(LuckyDayMoney<Stake){
+                    LuckyDay=day;
+                    UnluckyDay=month;
+                    LuckyDayMoney=Stake;
+                }
+                if(UnluckyDayMoney>Stake){
+                    UnluckyDay=day;
+                    UnluckyMonth=month;
+                    UnluckyDayMoney=Stake;
                 }
                 day++;
             }
             month++;
-            System.out.println();
         }
+        System.out.println("lucky day " +LuckyDay +"in month " +LuckyMonth+"max money Won is " + (LuckyDayMoney-Stake) +"rs");
+        System.out.println("unlucky day " +UnluckyDay +"in month " +UnluckyMonth+"max money lost is " + (Stake-LuckyDayMoney) +"rs");
+
     }
 
 }
